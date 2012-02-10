9d92603e0a695480bea5460aaa2ec03bfdc09ef1
Plamen Stoev <psstoev@gmail.com>
2012-02-10 05:07:40 +0200

    Remove unneeded else-s for better readability.

--- src/willy/views.py
+++ src/willy/views.py
@@ -19,26 +19,27 @@ def register(request):
         return render_to_response('register.html',
                                   {'form' : RegistrationForm()},
                                   context_instance=RequestContext(request))
-    else:
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user_data = {}
-            user_data['username'] = form.cleaned_data['username']
-            user_data['password'] = form.cleaned_data['password1']
-            user_data['email'] = form.cleaned_data['email']
-            user = User.objects.create_user(**user_data)
-            user.first_name = form.cleaned_data['first_name']
-            user.last_name = form.cleaned_data['last_name']
-            user.save()
 
-            category = Category(name=user.username, owner=user)
-            category.category_parent = category.id
-            category.save()
-            
-            user = authenticate(username=user_data['username'], password=user_data['password'])
-            login(request, user)
-            return redirect('/welcome/')
-        else:
-            return render_to_response('register.html',
-                                      {'form' : form},
-                                      context_instance=RequestContext(request))
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+        user_data = {}
+        user_data['username'] = form.cleaned_data['username']
+        user_data['password'] = form.cleaned_data['password1']
+        user_data['email'] = form.cleaned_data['email']
+        user = User.objects.create_user(**user_data)
+        user.first_name = form.cleaned_data['first_name']
+        user.last_name = form.cleaned_data['last_name']
+        user.save()
+
+        # Create a new category with the same name as the username
+        category = Category(name=user.username, owner=user)
+        category.category_parent = category.id
+        category.save()
+
+        user = authenticate(username=user_data['username'], password=user_data['password'])
+        login(request, user)
+        return redirect('/welcome/')
+
+    return render_to_response('register.html',
+                              {'form' : form},
+                              context_instance=RequestContext(request))
