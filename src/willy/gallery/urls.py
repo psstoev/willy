526cedf1194159e2c7ad5e71c418e199bda8da74
Plamen Stoev <psstoev@gmail.com>
2012-02-10 05:19:07 +0200

    Now users can edit existing categories.

--- src/willy/gallery/urls.py
+++ src/willy/gallery/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('gallery.views',
     # Category management:
     url(r'^category/add?', 'add_category'),
     url(r'^category/view/(?P<category_id>\d+)/?', 'view_category'),
+    url(r'^category/edit/(?P<category_id>\d+)/?', 'edit_category'),
 )
--- src/willy/gallery/views.py
+++ src/willy/gallery/views.py
@@ -10,6 +10,7 @@ def add_category(request):
         return render_to_response('add_category.html',
                                   {'form' : CategoryForm()},
                                   context_instance=RequestContext(request))
+
     form = CategoryForm(request.POST)
     if form.is_valid():
         category_data = form.cleaned_data
@@ -18,6 +19,32 @@ def add_category(request):
         category = Category(**category_data)
         category.save()
         return redirect('/welcome/')
+
+    return render_to_response('add_category.html',
+                              {'form' : form},
+                              context_instance=RequestContext(request))
+
+def edit_category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    if request.method == 'GET':
+        return render_to_response('edit_category.html',
+                                  {'form' : CategoryForm(instance=category),
+                                   'category_id' : category_id},
+                                  context_instance=RequestContext(request))
+
+    form = CategoryForm(request.POST)
+    if form.is_valid():
+        category_data = form.cleaned_data
+        
+        category.category_parent = category_data['category_parent']
+        category.name = category_data['name']
+        category.save()
+        return redirect('/welcome/')    
+
+    return render_to_response('edit_category.html',
+                              {'form' : form,
+                               'category_id' : category_id},
+                              context_instance=RequestContext(request))
 
 def view_category(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
