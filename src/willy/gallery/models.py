95a72d8ddaeabc7dbc04c511884a2ae35c83f343
Plamen Stoev <psstoev@gmail.com>
2012-02-10 02:36:22 +0200

    Add gallery to INSTALLED_APPS. Add Category model.

--- src/willy/gallery/models.py
+++ src/willy/gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    category_parent = models.ForeignKey('Category', related_name='parent')
+    owner = models.ForeignKey(User)
