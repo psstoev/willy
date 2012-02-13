204a0f691181e65bd0ce6fa72999bc85804689bd
Plamen Stoev <psstoev@gmail.com>
2012-02-13 23:07:57 +0200

    Fix lightbox, so the maximum width of the previewed picture is 960.

--- src/willy/static/js/jquery.lightbox-0.5.js
+++ src/willy/static/js/jquery.lightbox-0.5.js
@@ -137,7 +137,7 @@
 		 */
 		function _set_interface() {
 			// Apply the HTML markup into body tag
-			$('body').append('<div id="jquery-overlay"></div><div id="jquery-lightbox"><div id="lightbox-container-image-box"><div id="lightbox-container-image"><img id="lightbox-image"><div style="" id="lightbox-nav"><a href="#" id="lightbox-nav-btnPrev"></a><a href="#" id="lightbox-nav-btnNext"></a></div><div id="lightbox-loading"><a href="#" id="lightbox-loading-link"><img src="' + settings.imageLoading + '"></a></div></div></div><div id="lightbox-container-image-data-box"><div id="lightbox-container-image-data"><div id="lightbox-image-details"><div id="lightbox_picinfo"><p><span class="lightbox_label">Name:<span id="lightbox_picname"></span></span></p><p><span class="lightbox_label">Description:<span id="lightbox_picdesc"></span></span></p><p><span class="lightbox_label">Owner:<span id="lightbox_picowner"></span></span></p></div><span id="lightbox-image-details-caption"></span><span id="lightbox-image-details-currentNumber"></span></div><div id="lightbox-secNav"><a href="#" id="lightbox-secNav-btnClose"><img src="' + settings.imageBtnClose + '"></a></div></div></div></div>');	
+			$('body').append('<div id="jquery-overlay"></div><div id="jquery-lightbox"><div id="lightbox-container-image-box"><div id="lightbox-container-image"><img id="lightbox-image"><div style="" id="lightbox-nav"><a href="#" id="lightbox-nav-btnPrev"></a><a href="#" id="lightbox-nav-btnNext"></a></div><div id="lightbox-loading"><a href="#" id="lightbox-loading-link"><img src="' + settings.imageLoading + '"></a></div></div></div><div id="lightbox-container-image-data-box"><div id="lightbox-container-image-data"><div id="lightbox-image-details"><div id="lightbox_picinfo"><p><span class="lightbox_label">Name:</span><span id="lightbox_picname" class="lightbox_picdata"></span></p><p><span class="lightbox_label">Description:</span><span id="lightbox_picdesc" class="lightbox_picdata"></span></p><p><span class="lightbox_label">Owner:</span><span id="lightbox_picowner" class="lightbox_picdata"></span></p></div><span id="lightbox-image-details-caption"></span><span id="lightbox-image-details-currentNumber"></span></div><div id="lightbox-secNav"><a href="#" id="lightbox-secNav-btnClose"><img src="' + settings.imageBtnClose + '"></a></div></div></div></div>');	
 			// Get page sizes
 			var arrPageSizes = ___getPageSize();
 			// Style overlay and show it
@@ -213,6 +213,11 @@
 		 */
 		function _resize_container_image_box(intImageWidth,intImageHeight) {
 			// Get current width and height
+                    if (intImageWidth > 960) {
+            	        var scale = intImageWidth / 960;
+            	        intImageWidth = 960;
+            	        intImageHeight = intImageHeight / scale;
+                    }
 			var intCurrentWidth = $('#lightbox-container-image-box').width();
 			var intCurrentHeight = $('#lightbox-container-image-box').height();
 			// Get the width and height of the selected image plus the padding
