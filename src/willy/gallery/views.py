266daecf88826151e4444b2f88881d60483c5845
Plamen Stoev <psstoev@gmail.com>
2012-02-13 23:31:15 +0200

    Add pagination to view_pictures.html.

--- src/willy/gallery/views.py
+++ src/willy/gallery/views.py
@@ -10,6 +10,7 @@ from django.views.decorators.csrf import csrf_protect
 from django.utils.translation import ugettext as _
 from django.core.files import File
 from django.conf import settings
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from willy.gallery.models import Category, Picture
 from willy.gallery.forms import CategoryForm, PictureUploadForm, PictureEditForm
@@ -254,11 +255,23 @@ def view_categories(request):
                                   
 def view_pictures(request):
     categories = get_categories(request)
-    pictures = Picture.objects.all()
-    if request.method == 'GET':
-        return render_to_response('view_pictures.html',
-                                  {'user' : request.user,
-                                  'pictures' : pictures,
-                                  'categories' : categories
-                                  },
-                                  context_instance=RequestContext(request))
+    paginator = Paginator(Picture.objects.all(), 1)
+
+    # Make sure page request is an int. If not, deliver first page.
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    # If page request (9999) is out of range, deliver last page of results.
+    try:
+        pictures = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        pictures = paginator.page(paginator.num_pages)
+
+    return render_to_response('view_pictures.html',
+                              {'user' : request.user,
+                               'pictures' : pictures,
+                               'categories' : categories,
+                               },
+                              context_instance=RequestContext(request))
